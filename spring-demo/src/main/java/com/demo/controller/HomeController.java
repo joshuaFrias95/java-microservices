7221bc7f56d12e08329367cca17b7b994dc8fbc7
Joshua Frías <metatron1495@gmail.com>
2024-03-14 21:41:45 -0600

    Add: Ejemplo de endpoint que retorna un objeto

--- spring-demo/src/main/java/com/demo/controller/HomeController.java
+++ spring-demo/src/main/java/com/demo/controller/HomeController.java
@@ -1,15 +1,25 @@
 package com.demo.controller;
 
-import org.springframework.stereotype.Controller;
+import com.demo.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
 @RestController
 public class HomeController {
 
     @RequestMapping("/")
     public String home() {
         return "Home";
+    }
+
+    @GetMapping("/user")
+    public User user() {
+        User user = new User();
+        user.setId("1");
+        user.setName("John");
+        user.setEmailId("johnDoe@mail.com");
+
+        return user;
     }
 }
