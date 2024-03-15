d0e2827fd4f13251e1b47b13a28f8a02fab78b91
Joshua Frías <metatron1495@gmail.com>
2024-03-14 22:16:14 -0600

    Add: RequestParam example

--- spring-demo/src/main/java/com/demo/controller/HomeController.java
+++ spring-demo/src/main/java/com/demo/controller/HomeController.java
@@ -1,10 +1,7 @@
 package com.demo.controller;
 
 import com.demo.model.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HomeController {
@@ -26,7 +23,12 @@ public class HomeController {
 
     @GetMapping("/{id}/{var}")
     public String pathVariable(@PathVariable String id, @PathVariable("var") String name) {
-
         return "ID:" + id + "\nNombre: " + name;
+    }
+
+    @GetMapping("/requestParam")
+    public String requestParams(@RequestParam String name,
+                                @RequestParam(name = "email", required = false, defaultValue = "") String emailId) {
+        return "Your name is " + name + "and your email is: " + emailId;
     }
 }
