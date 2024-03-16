95c44ad1bcb1a283040306ae58f85970d7e25755
Joshua Frías <metatron1495@gmail.com>
2024-03-16 17:18:13 -0600

    Refactor: Versionado de api

--- spring-demo/src/main/java/com/demo/controller/v1/EmployeeV1Controller.java
+++ spring-demo/src/main/java/com/demo/controller/v1/EmployeeV1Controller.java
@@ -1,4 +1,4 @@
-package com.demo.controller;
+package com.demo.controller.v1;
 
 import com.demo.model.Employee;
 import com.demo.service.EmployeeService;
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/employees")
-public class EmployeeController {
+@RequestMapping("/v1/employees")
+public class EmployeeV1Controller {
 
     @Autowired
     private EmployeeService employeeService;
