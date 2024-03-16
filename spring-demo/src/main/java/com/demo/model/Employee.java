700d55ddeedd9edc0ab85098acdbc23dceb00cb1
Joshua Frías <metatron1495@gmail.com>
2024-03-16 17:04:54 -0600

    Add: Filtrado de información en peticinoes json y xml

--- spring-demo/src/main/java/com/demo/model/Employee.java
+++ spring-demo/src/main/java/com/demo/model/Employee.java
@@ -1,11 +1,17 @@
 package com.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties({"department", "emailId"}) // Filtramos atributos a nivel de clase
 public class Employee {
 
     private String employeeID;
     private String firstName;
     private String lastName;
     private String emailId;
+
+    // @JsonIgnore -> Filtramos campos a nivel de atributos
     private String department;
 
     public String getEmployeeID() {
